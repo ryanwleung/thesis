5509d9682560812434e8aea86332d293f73dbfaa
Ryan Leung <ryanwleung@utexas.edu>
2018-10-13 18:16:18 -0500

    final commit

--- mainScript.m
+++ mainScript.m
@@ -13,7 +13,7 @@ clear variables
 % DCTheoreticalFormation.PlotMethaneQuantities(seafloorDepthArray, minQuantityToFracture2PList, minQuantityToFracture3PList);
 % DCTheoreticalFormation.PlotGasSaturations(seafloorDepthArray, sgFracture2PList, sgFracture3PList);
 % DCTheoreticalFormation.PlotDepths(seafloorDepthArray, depthStructList);
-
+% 
 % PlotAllMICP()
 
 
@@ -77,19 +77,19 @@ clear variables
 % ch4Quantity = 55; % near max for HR
 % ch4Quantity = 120; % near max for BR
 
-% ch4Quantity = 40;
-% % 
-% % obj = DCBlakeRidge();
-% % obj = DCHydrateRidge();
-% obj = DCKumanoBasin();
+ch4Quantity = 40;
 % 
-% [exportTable, transitionZoneProperties] = obj.RunSolubilitySaturationRoutine(ch4Quantity);
-% exportTable = obj.RunRockAndRatioRoutine(exportTable);
-% 
-% obj.GenerateResultPlots(exportTable, transitionZoneProperties);
+% obj = DCBlakeRidge();
+% obj = DCHydrateRidge();
+obj = DCKumanoBasin();
+
+[exportTable, transitionZoneProperties] = obj.RunSolubilitySaturationRoutine(ch4Quantity);
+exportTable = obj.RunRockAndRatioRoutine(exportTable);
+
+obj.GenerateResultPlots(exportTable, transitionZoneProperties);
 
 
-%%% Plot results
+% %% Plot results
 % obj.PlotMICP();
 % obj.PlotCumPSD();
 % obj.PlotPSD('linear');
@@ -107,41 +107,41 @@ clear variables
 
 
 
-%% Seismic analysis for Blake Ridge
-obj = DCSeismicAnalysisBR();
-
-% test = 1;
-% obj.clayK = 10.5e9; % Pa
-% obj.parameterSensitivitySeismogramAxis = [4.17 4.31 -0.11 0.06];
-
-
-test = 2;
-% obj.clayK = 10.5e9; % Pa
-% obj.seismogramAxis = [4.21 4.35 -0.15 0.1];
-% obj.seismogramDickensAxis = [4.21 4.35 -0.15 0.15];
-
-switch test
-    case 1
-        [ WaveParameterSensitivity , ~ , WaveBaseParameterSensitivity , dataBase , ~ ] = obj.RunSeismicAnalysisRoutine('ParameterSensitivity');
-
-        obj.PlotParameterSensitivity(WaveParameterSensitivity, WaveBaseParameterSensitivity)
-%         obj.PlotPhaseSaturations()
-        obj.PlotBackgroundProperties(dataBase)
-%         obj.PlotThicknessVsQuantity(WaveParameterSensitivity)
-%         obj.PlotPeakAmplitudeRatio(WaveParameterSensitivity)
-        obj.PlotTWTTLength(WaveParameterSensitivity)
-        
-    case 2
-        obj.Dickens = obj.LoadDickensBlakeRidge();
-        [ WaveOriginalResolution , data , WaveBaseOriginalResolution , ~ , WaveDickens ] = obj.RunSeismicAnalysisRoutine('OriginalResolution');
-        
-        obj.PlotSeismogramOriginalResolution(WaveOriginalResolution)
-        obj.PlotVelocityStructureOriginalResolution(WaveBaseOriginalResolution, WaveOriginalResolution)
-%         obj.PlotPeakAmplitudeRatio(WaveOriginalResolution)
-        obj.PlotTWTTLength(WaveOriginalResolution)
-        obj.PlotDickensSeismogram(WaveOriginalResolution, WaveDickens)
-        obj.PlotAcousticImpedance(WaveOriginalResolution)
-end
+% %% Seismic analysis for Blake Ridge
+% obj = DCSeismicAnalysisBR();
+% 
+% % test = 1;
+% % obj.clayK = 10.5e9; % Pa
+% % obj.parameterSensitivitySeismogramAxis = [4.17 4.31 -0.11 0.06];
+% 
+% 
+% test = 2;
+% % obj.clayK = 10.5e9; % Pa
+% % obj.seismogramAxis = [4.21 4.35 -0.15 0.1];
+% % obj.seismogramDickensAxis = [4.21 4.35 -0.15 0.15];
+% 
+% switch test
+%     case 1
+%         [ WaveParameterSensitivity , ~ , WaveBaseParameterSensitivity , dataBase , ~ ] = obj.RunSeismicAnalysisRoutine('ParameterSensitivity');
+% 
+%         obj.PlotParameterSensitivity(WaveParameterSensitivity, WaveBaseParameterSensitivity)
+% %         obj.PlotPhaseSaturations()
+%         obj.PlotBackgroundProperties(dataBase)
+% %         obj.PlotThicknessVsQuantity(WaveParameterSensitivity)
+% %         obj.PlotPeakAmplitudeRatio(WaveParameterSensitivity)
+%         obj.PlotTWTTLength(WaveParameterSensitivity)
+%         
+%     case 2
+%         obj.Dickens = obj.LoadDickensBlakeRidge();
+%         [ WaveOriginalResolution , data , WaveBaseOriginalResolution , ~ , WaveDickens ] = obj.RunSeismicAnalysisRoutine('OriginalResolution');
+%         
+%         obj.PlotSeismogramOriginalResolution(WaveOriginalResolution)
+%         obj.PlotVelocityStructureOriginalResolution(WaveBaseOriginalResolution, WaveOriginalResolution)
+% %         obj.PlotPeakAmplitudeRatio(WaveOriginalResolution)
+%         obj.PlotTWTTLength(WaveOriginalResolution)
+%         obj.PlotDickensSeismogram(WaveOriginalResolution, WaveDickens)
+%         obj.PlotAcousticImpedance(WaveOriginalResolution)
+% end
 
 
 % ch4Quantity = (1 : 1 : 40)';
